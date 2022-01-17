be6b1f4bf8b3688dde09fdb6574b9a55213649a7
Rogelio Galindo <roge.galindo@gmail.com>
2022-01-17 13:22:45 -0600

    before vic

--- scripts2.js
+++ scripts2.js
@@ -1,8 +1,7 @@
 
 
 function pagoMensual() {
-
-    
+  
   let num1 = document.getElementById("montoDeseado").value;
   let num2 = document.getElementById("plazoMeses").value;
   let num3 = document.getElementById("tasaAnual").value;
@@ -11,8 +10,6 @@ function pagoMensual() {
   let divide = ((firstOp * num3 )/ 100) + firstOp;
 
   document.getElementById("result").innerHTML = divide;
-
-
 }
     
 
@@ -32,10 +29,34 @@ function totalPagado() {
   let num3 = document.getElementById("tasaAnual").value;
 
   let x = parseFloat(num1) + parseFloat(num3) + parseFloat(apertura);
-
-
-    document.getElementById("paided").innerHTML = x;
-
   
+  document.getElementById("paided").innerHTML = x;
+
 }
-  
+
+function info() {
+  let rango = document.getElementById("montoDeseado").value;
+  let num2 = document.getElementById("plazoMeses").value;
+  let num3 = document.getElementById("tasaAnual").value;
+
+  let y = parseFloat(rango)  / num2 ;
+  let q = (y * parseFloat(num3))/100  + y ;
+
+  document.getElementById("ahuevo").innerHTML = q;
+}
+
+function mensualidad() {
+  // let button = 12;
+  let button = document.getElementsByName("hola");
+
+  let opi= button ;
+
+  document.getElementById("premium").innerHTML = opi;
+
+
+}
+
+
+function submit() {
+  pagoMensual(); montoNeto(); totalPagado() ; info(); 
+}
